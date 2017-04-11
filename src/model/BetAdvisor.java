81da759b768b949711cbea0520287651a2c137a7
tonussi <lptonussi@gmail.com>
2017-04-11 19:48:25 -0300

    Add statistics methods

--- src/model/BetAdvisor.java
+++ src/model/BetAdvisor.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class BetAdvisor {
@@ -27,4 +29,39 @@ public class BetAdvisor {
 	private static int generateProbabilisticBetValue(List<Round> roundHistory) {
 		return 0;
 	}
+
+	private static Map<Integer, Integer> findFrequencies(List<Round> roundHistory) {
+		int listOfSums[] = new int[roundHistory.size()];
+		int minimumValue = Integer.MAX_VALUE, maximumValue = 0, squareRoot = 0;
+
+		squareRoot = (int) Math.sqrt(roundHistory.size());
+
+		/* Transform list of sums of chop-sticks to array
+		   Get minimum value and maximum value. */
+		for (int i = 0; i < roundHistory.size(); i++) {
+			listOfSums[i] = roundHistory.get(i).getChopsticks();
+			minimumValue = Math.min(minimumValue, listOfSums[i]);
+			maximumValue = Math.max(maximumValue, listOfSums[i]);
+		}
+
+		int difference = maximumValue - minimumValue;
+		int classInterval = difference / squareRoot;
+
+		return groupAndMapFrequencies(listOfSums);
+	}
+
+	private static Map<Integer, Integer> groupAndMapFrequencies(int[] listOfSums) {
+		Map<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < listOfSums.length; i++) {
+			if (!frequencies.containsKey(listOfSums[i])) {
+				frequencies.put(listOfSums[i], 1);
+			} else {
+				frequencies.put(listOfSums[i], frequencies.get(listOfSums[i]) + 1);
+			}
+		}
+
+		return frequencies;
+	}
+
 }
